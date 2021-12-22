d67b4564106267d93722ec46d449c4c63d4cfe86
Julian Wolf <julian.w16@gmail.com>
2021-12-22 17:20:31 +0200

    Init bot things

---
+++ bot.py
@@ -0,0 +1,53 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+bot = commands.Bot(command_prefix='!')
+
+
+from random import choice
+
+LIST_OF_MAPS = [
+    'Tanglewood Street House',
+    'Ridgeview Road House',
+    'Bleasdale Farmhouse',
+    'Grafton Farmhouse',
+    'Edgefield Street House',
+    'Willow Street House',
+    'Maple Lodge Campsite'
+]
+
+DIFFICULTY = [
+    'Amateur',
+    'Intermediate',
+    'Professional',
+    'Nightmare'
+]
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!select map':
+        response = choice(LIST_OF_MAPS)
+        await message.channel.send(f'{message.author} has requested the ether to choose a map, and they chose **{response}**!')
+
+    if message.content == '!select map and difficulty':
+        response = choice(LIST_OF_MAPS)
+        difficulty = choice(DIFFICULTY)
+        await message.channel.send(f'_{message.author}_ has requested the ether to choose a map, and they chose **{response}** with difficulty of **{difficulty}**!')
+
+
+client.run(TOKEN)
